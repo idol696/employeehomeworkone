41f11554d49ce68d5c83d2f1479cc56a988b9587
Roman Kuzmin <ghelatin@mail.ru>
2024-08-11 12:52:27 +0300

    commit init

---
+++ src/main/java/ru/prostostudia/employeehomeworkone/EmployeehomeworkoneApplication.java
@@ -0,0 +1,13 @@
+package ru.prostostudia.employeehomeworkone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeehomeworkoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeehomeworkoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/prostostudia/employeehomeworkone/EmployeehomeworkoneApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.prostostudia.employeehomeworkone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeehomeworkoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
